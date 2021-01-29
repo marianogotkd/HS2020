c550d993caa762ce658ed656c6bfed579ad4b4d1
hamersoft <marianogo@hotmail.com>
2021-01-29 14:24:13 -0300

    sad

--- Dialisis/Aplicacion/Modulos/Enfermeria/Ds_enfermeria.vb
+++ Dialisis/Aplicacion/Modulos/Enfermeria/Ds_enfermeria.vb
@@ -1,13 +1,1 @@
-﻿Partial Class Ds_enfermeria
-    Partial Class PredefinidosDataTable
-
-        Private Sub PredefinidosDataTable_ColumnChanging(ByVal sender As System.Object, ByVal e As System.Data.DataColumnChangeEventArgs) Handles Me.ColumnChanging
-            If (e.Column.ColumnName = Me.Predef_CantidadColumn.ColumnName) Then
-                'Agregar código de usuario aquí
-            End If
-
-        End Sub
-
-    End Class
-
-End Class
+﻿
