2fb22f61fc5fed64041c4bbc895abaa6e14713a6
hamersoft <marianogo@hotmail.com>
2021-02-04 19:23:43 -0300

    5465

--- Dialisis/Aplicacion/Modulos/Enfermeria/Insumos_Predefinidos.vb
+++ Dialisis/Aplicacion/Modulos/Enfermeria/Insumos_Predefinidos.vb
@@ -93,4 +93,135 @@
     End Sub
 
 
+#Region "Validacion de grilla - solo numeros en celda"
+    Private Sub DataGridView1_EditingControlShowing(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewEditingControlShowingEventArgs) Handles DataGridView1.EditingControlShowing
+
+        '    valido_fraccionable(datagridview_Predef.CurrentRow.Cells("columna_codinterno").Value)
+
+        'referencia a la celda
+        Dim validar As TextBox = CType(e.Control, TextBox)
+        'agregar el controlador de eventos para el keypress
+        '   AddHandler validar.KeyPress, AddressOf validar_Keypress
+    End Sub
+    'Private Sub valido_fraccionable(ByVal cod_ingresado As String)
+    '    'Encontrado = "no"
+    '    Dim i As Integer = 0
+    '    While i < VentaGestion_DS_PROD.Tables(1).Rows.Count
+    '        Dim cod_interno As String = VentaGestion_DS_PROD.Tables(1).Rows(i).Item("prod_codinterno").ToString
+    '        If cod_ingresado = cod_interno Then
+    '            '' si es fraccionable o no el producto solamente por este if pasa''
+    '            tipo_prod = VentaGestion_DS_PROD.Tables(1).Rows(i).Item("prod_tipo").ToString
+    '            '''''''''''''''''''''''''''''MARIANO 17/6/19'''''''''''''''''''''''
+    '            'Dim index As Integer = DataGridView1.CurrentRow.Index
+    '            'DataGridView1.CurrentRow.Cells("item_num").Value = CInt(DataGridView1.CurrentRow.Index + 1)
+    '            'DataGridView1.CurrentRow.Cells("prod_id").Value = DS_PROD.Tables(0).Rows(i).Item("prod_id").ToString
+    '            'DataGridView1.CurrentRow.Cells("prod_codinterno").Value = DS_PROD.Tables(0).Rows(i).Item("prod_codinterno").ToString
+    '            'DataGridView1.CurrentRow.Cells("prod_descripcion").Value = DS_PROD.Tables(0).Rows(i).Item("prod_descripcion").ToString
+    '            'DataGridView1.CurrentRow.Cells("Cantidad").Value = CInt(1)
+    '            'Encontrado = "si"
+    '        End If
+    '        i = i + 1
+    '    End While
+    '    'If Encontrado = "no" Then
+    '    '    'buscar por codigo de barras
+    '    '    DataGridView1.CurrentRow.Cells("item_num").Value = ""
+    '    '    DataGridView1.CurrentRow.Cells("prod_id").Value = ""
+    '    '    DataGridView1.CurrentRow.Cells("prod_codinterno").Value = ""
+    '    '    DataGridView1.CurrentRow.Cells("prod_descripcion").Value = ""
+    '    '    DataGridView1.CurrentRow.Cells("Cantidad").Value = ""
+    '    '    MessageBox.Show("No existe el producto para ese proveedor", "Sistema de Gestión")
+    '    'End If
+    '    'If DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(2).Value <> "" Then
+    '    '    DataGridView1.Rows.Add()
+    '    'End If
+    'End Sub
+    'Private Sub validar_Keypress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs)
+
+
+    '    Dim columna As Integer = datagridview_Predef.CurrentCell.ColumnIndex
+
+    '    Dim variasdasd = datagridview_Predef.CurrentRow.Cells("columna_codinterno").Value
+
+    '    If CStr(datagridview_Predef.CurrentRow.Cells("columna_codinterno").Value) = "" And (columna = 5 Or columna = 6) Then 'la columna 5 es Cantidad, la columna 6 es descuento
+    '        'If IsDBNull(DataGridView1.CurrentRow.Cells("columna_codinterno").Value) And columna = 5 Then
+    '        e.Handled = True
+    '    Else
+    '        If datagridview_Predef.CurrentRow.Cells("columna_prod_id").Value <> "" And columna = 2 Then
+    '            e.Handled = True
+    '            If Char.IsControl(e.KeyChar) Then
+    '                datagridview_Predef.CurrentRow.Cells("columna_codinterno").Value = celda_codinterno
+    '            End If
+
+
+    '        Else
+    '            e.Handled = True 'bloqueo todo ingreso en primera instancia
+    '            If Char.IsNumber(e.KeyChar) Then
+    '                e.Handled = False 'aqui habilito si es numero
+    '            End If
+    '            If Char.IsControl(e.KeyChar) Then
+    '                e.Handled = False 'aqui habilito si es una operacion de control como "borrar"
+    '            End If
+    '            'If e.KeyChar <> "" Then 'si es distinto de vacio valido 
+    '            '    If Me.DataGridView1.CurrentCell.ColumnIndex <> 1 Then 'si no es la columna 1, solo numeros y comas
+    '            '        e.Handled = True 'bloqueo todo ingreso en primera instancia
+    '            '        If Char.IsNumber(e.KeyChar) Then
+    '            '            e.Handled = False 'aqui habilito si es numero
+    '            '        End If
+    '            '        If Char.IsControl(e.KeyChar) Then
+    '            '            e.Handled = False 'aqui habilito si es una operacion de control como "borrar"
+    '            '        End If
+
+    '            '        'obtener indice de la columna
+    '            '        Dim columna As Integer = Me.DataGridView1.CurrentCell.ColumnIndex
+    '            '        'comprobar si la celda en edicion corresponde a la columnas deseadas
+    '            '        'If columna = 1 Or columna = 2 Then
+    '            '        'obtener caracter
+    '            If columna = 5 Then
+    '                If tipo_prod = "Fraccionable" Then
+    '                    Dim caracter As Char = e.KeyChar
+    '                    '        'referencia a la celda
+    '                    Dim txt As TextBox = CType(sender, TextBox)
+    '                    'aqui pongo el codigo para remplazar el punto por una coma
+    '                    If txt.ToString() = "." Then
+    '                        txt.Text = ","
+    '                    End If
+    '                    If caracter.ToString() = "." Then
+    '                        caracter = ","
+    '                    End If
+    '                    'comprobar si el caracter es un número o el retroceso, si el caracter es un separador decimal y que no contiene ya el separador
+    '                    If (Char.IsNumber(caracter)) Or (caracter = ChrW(Keys.Back)) Or (caracter = ",") And (txt.Text.Contains(",") = False) Then
+    '                        e.KeyChar = caracter
+    '                        e.Handled = False
+    '                    Else
+    '                        e.Handled = True
+    '                    End If
+    '                End If
+    '            End If
+    '            If columna = 6 Then 'la columna 6 es descuento, que acepta decimales
+    '                Dim caracter As Char = e.KeyChar
+    '                '        'referencia a la celda
+    '                Dim txt As TextBox = CType(sender, TextBox)
+    '                'aqui pongo el codigo para remplazar el punto por una coma
+    '                If txt.ToString() = "." Then
+    '                    txt.Text = ","
+    '                End If
+    '                If caracter.ToString() = "." Then
+    '                    caracter = ","
+    '                End If
+    '                'comprobar si el caracter es un número o el retroceso, si el caracter es un separador decimal y que no contiene ya el separador
+    '                If (Char.IsNumber(caracter)) Or (caracter = ChrW(Keys.Back)) Or (caracter = ",") And (txt.Text.Contains(",") = False) Then
+    '                    e.KeyChar = caracter
+    '                    e.Handled = False
+    '                Else
+    '                    e.Handled = True
+    '                End If
+    '            End If
+    '        End If
+    '    End If
+    'End Sub
+
+#End Region
+
+
+
 End Class
--- SistemaSantaFe/Datos/Conexion.vb
+++ SistemaSantaFe/Datos/Conexion.vb
@@ -12,10 +12,10 @@ Public Class Conexion
     'Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Password=123choco;Persist Security Info=True;User ID=choco;Initial Catalog=SantaFe;Data Source=25.112.197.145\SQLEXPRESS_CHOK")
 
     'Conexion local NoteMariano
-    Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=SantaFe;Data Source=(local)")
+    'Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=SantaFe;Data Source=(local)")
 
     'Conexion MDA Electricidad
-    'Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=SantaFe;Data Source=DESKTOP-CK4O564\SQLEXPRESS")
+    Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=SantaFe;Data Source=DESKTOP-CK4O564\SQLEXPRESS")
 
 
 End Class
